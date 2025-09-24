b1dc1bba1830a2fc9a42b3e4fd59cea9f4145945
MuslemRahimi <mrahimi@stocknear.com>
2025-09-24 16:46:05 +0200

    add reasoning

--- src/lib/components/Chat/Assistant.svelte
+++ src/lib/components/Chat/Assistant.svelte
@@ -7,6 +7,7 @@
   import { page } from "$app/stores";
   import { browser } from "$app/environment";
   import { derived } from "svelte/store";
+  import { chatReasoning } from "$lib/store";
 
   // Lazy load components
   import ChatMessage from "$lib/components/Chat/ChatMessage.svelte";
@@ -227,14 +228,6 @@
     }, 450);
   }
 
-  function toggleChat() {
-    isOpen ? closeChat() : openChat();
-  }
-
-  function minimizeChat() {
-    isMinimized = !isMinimized;
-  }
-
   function toggleFullscreen() {
     isFullscreen = !isFullscreen;
   }
@@ -431,7 +424,11 @@
       const res = await fetch("/api/chat", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ query: userQuery, chatId: chatId }),
+        body: JSON.stringify({
+          query: userQuery,
+          chatId: chatId,
+          reasoning: $chatReasoning,
+        }),
       });
 
       if (!res.ok || !res.body) {
@@ -876,6 +873,15 @@
     }, 100);
   }
 
+  function saveSettings() {
+    if (localStorage && typeof localStorage !== "undefined") {
+      localStorage.setItem(
+        "chat-settings",
+        JSON.stringify({ reasoning: $chatReasoning }),
+      );
+    }
+  }
+
   onMount(() => {
     if (browser) {
       document.addEventListener("mousemove", handleDrag);
@@ -1226,22 +1232,25 @@
                       <DropdownMenu.Trigger asChild let:builder>
                         <Button
                           builders={[builder]}
-                          class="w-full border-gray-300 font-medium dark:border-gray-600 border bg-black hover:bg-gray-800 text-white dark:bg-white dark:hover:bg-gray-100 dark:text-black ease-out flex flex-row justify-between items-center px-2 py-1 rounded truncate transition-colors"
+                          class="w-full bg-white dark:bg-secondary text-gray-800 sm:hover:text-black dark:text-gray-200 dark:sm:hover:text-white ease-out flex flex-row justify-between items-center  rounded truncate"
                         >
-                          <span class="truncate text-xs">@Agents</span>
                           <svg
-                            class="-mr-1 ml-2 h-5 w-5 inline-block"
-                            viewBox="0 0 20 20"
-                            fill="currentColor"
-                            style="max-width:40px"
-                            aria-hidden="true"
-                          >
-                            <path
+                            class="size-4.5"
+                            viewBox="0 0 15 15"
+                            fill="none"
+                            xmlns="http://www.w3.org/2000/svg"
+                            ><path
+                              d="M5.5 3C4.67157 3 4 3.67157 4 4.5C4 5.32843 4.67157 6 5.5 6C6.32843 6 7 5.32843 7 4.5C7 3.67157 6.32843 3 5.5 3ZM3 5C3.01671 5 3.03323 4.99918 3.04952 4.99758C3.28022 6.1399 4.28967 7 5.5 7C6.71033 7 7.71978 6.1399 7.95048 4.99758C7.96677 4.99918 7.98329 5 8 5H13.5C13.7761 5 14 4.77614 14 4.5C14 4.22386 13.7761 4 13.5 4H8C7.98329 4 7.96677 4.00082 7.95048 4.00242C7.71978 2.86009 6.71033 2 5.5 2C4.28967 2 3.28022 2.86009 3.04952 4.00242C3.03323 4.00082 3.01671 4 3 4H1.5C1.22386 4 1 4.22386 1 4.5C1 4.77614 1.22386 5 1.5 5H3ZM11.9505 10.9976C11.7198 12.1399 10.7103 13 9.5 13C8.28967 13 7.28022 12.1399 7.04952 10.9976C7.03323 10.9992 7.01671 11 7 11H1.5C1.22386 11 1 10.7761 1 10.5C1 10.2239 1.22386 10 1.5 10H7C7.01671 10 7.03323 10.0008 7.04952 10.0024C7.28022 8.8601 8.28967 8 9.5 8C10.7103 8 11.7198 8.8601 11.9505 10.0024C11.9668 10.0008 11.9833 10 12 10H13.5C13.7761 10 14 10.2239 14 10.5C14 10.7761 13.7761 11 13.5 11H12C11.9833 11 11.9668 10.9992 11.9505 10.9976ZM8 10.5C8 9.67157 8.67157 9 9.5 9C10.3284 9 11 9.67157 11 10.5C11 11.3284 10.3284 12 9.5 12C8.67157 12 8 11.3284 8 10.5Z"
+                              fill="currentColor"
                               fill-rule="evenodd"
-                              d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
                               clip-rule="evenodd"
-                            ></path>
-                          </svg>
+                            ></path></svg
+                          >
+                          <div class="flex items-center gap-0.5">
+                            <span class="max-w-16 truncate"
+                              ><span class="truncate"></span></span
+                            >
+                          </div>
                         </Button>
                       </DropdownMenu.Trigger>
                       <DropdownMenu.Content
@@ -1252,7 +1261,93 @@
                         class="w-64 h-fit max-h-56 overflow-y-auto scroller"
                       >
                         {#if selectedGroup === "overview"}
+                          <DropdownMenu.Label
+                            class="text-muted dark:text-gray-400 font-semibold dark:font-normal text-xs"
+                          >
+                            {data?.user?.credits} Credits left
+                          </DropdownMenu.Label>
+
+                          <DropdownMenu.Item
+                            class="sm:hover:bg-gray-200 dark:sm:hover:bg-primary"
+                          >
+                            <label
+                              on:click|capture={(event) => {
+                                event.preventDefault();
+                                $chatReasoning = !$chatReasoning;
+                                saveSettings();
+                              }}
+                              class="inline-flex justify-between w-full items-center cursor-pointer"
+                            >
+                              <span class="mr-1 text-sm">Reasoning</span>
+
+                              <div class="relative ml-auto">
+                                <input
+                                  type="checkbox"
+                                  checked={$chatReasoning}
+                                  class="sr-only peer"
+                                />
+                                <div
+                                  class="w-9 h-5 bg-gray-400 rounded-full peer peer-checked:bg-blue-600
+      after:content-[''] after:absolute after:top-0.5 after:left-[2px]
+      after:bg-white after:border-gray-300 after:border
+      after:rounded-full after:h-4 after:w-4 after:transition-all
+      peer-checked:after:translate-x-full"
+                                ></div>
+                              </div></label
+                            >
+                          </DropdownMenu.Item>
+
                           <DropdownMenu.Group>
+                            <DropdownMenu.Item
+                              on:click={(e) => {
+                                e.preventDefault();
+                                selectedGroup = "stockAgents";
+                              }}
+                              class="cursor-pointer sm:hover:bg-gray-300 dark:sm:hover:bg-primary"
+                            >
+                              <div
+                                class="flex flex-row items-center w-full text-sm"
+                              >
+                                <span>Stock Agents</span>
+                              </div>
+                              <svg
+                                class="ml-auto h-5 w-5 inline-block rotate-270"
+                                viewBox="0 0 20 20"
+                                fill="currentColor"
+                                style="max-width:40px"
+                                aria-hidden="true"
+                              >
+                                <path
+                                  fill-rule="evenodd"
+                                  d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                                  clip-rule="evenodd"
+                                ></path>
+                              </svg>
+                            </DropdownMenu.Item>
+                          </DropdownMenu.Group>
+                        {:else if selectedGroup === "stockAgents"}
+                          <DropdownMenu.Group>
+                            <div class="w-full p-1 flex items-stretch gap-1">
+                              <button
+                                type="button"
+                                on:click={(e) => {
+                                  e.preventDefault();
+                                  selectedGroup = "overview";
+                                }}
+                                class="aspect-square flex items-center cursor-pointer"
+                                ><svg
+                                  xmlns="http://www.w3.org/2000/svg"
+                                  class="w-5 h-5"
+                                  viewBox="0 0 24 24"
+                                  fill="none"
+                                  stroke="currentColor"
+                                  stroke-width="2"
+                                  stroke-linecap="round"
+                                  stroke-linejoin="round"
+                                  ><path d="m15 18-6-6 6-6"></path></svg
+                                ></button
+                              >
+                            </div>
                             {#each agentCategory as option}
                               <DropdownMenu.Item
                                 on:click={(e) => {
@@ -1315,7 +1410,7 @@
                                 type="button"
                                 on:click={(e) => {
                                   e.preventDefault();
-                                  selectedGroup = "overview";
+                                  selectedGroup = "stockAgents";
                                 }}
                                 class="aspect-square flex items-center cursor-pointer"
                                 ><svg
@@ -1355,17 +1450,6 @@
                       </DropdownMenu.Content>
                     </DropdownMenu.Root>
                   </div>
-
-                  {#if userData}
-                    <label
-                      class="ml-auto mr-2 whitespace-nowrap w-auto text-xs border-gray-300 font-medium dark:border-gray-600 border bg-inherit text-gray-600 dark:text-gray-300 flex flex-row justify-between items-center px-3 py-2 rounded"
-                    >
-                      <div>
-                        {userData?.credits?.toLocaleString("en-US")}
-                        <span class="hidden sm:inline-block">Credits</span>
-                      </div>
-                    </label>
-                  {/if}
 
                   <button
                     on:click={() =>
--- src/routes/chat/+page.svelte
+++ src/routes/chat/+page.svelte
@@ -12,6 +12,7 @@
   import { EditorView, Decoration, DecorationSet } from "prosemirror-view";
   import { keymap } from "prosemirror-keymap";
   import { agentOptions, agentCategory } from "$lib/utils";
+  import { chatReasoning } from "$lib/store";
 
   import { schema } from "prosemirror-schema-basic";
 
@@ -143,6 +144,15 @@
       },
     },
   });
+
+  function saveSettings() {
+    if (localStorage && typeof localStorage !== "undefined") {
+      localStorage.setItem(
+        "chat-settings",
+        JSON.stringify({ reasoning: $chatReasoning }),
+      );
+    }
+  }
 
   onMount(() => {
     editorView = new EditorView(editorDiv, {
@@ -537,32 +547,35 @@
                   class="relative min-h-12 h-auto overflow-y-hidden w-full outline-none"
                 >
                   <div
-                    class="absolute bottom-0 flex flex-row justify-end w-full bg-white dark:bg-[#2A2E39]"
+                    class="absolute bottom-0 flex flex-row justify-end w-full bg:inherit dark:bg-[#2A2E39]"
                   >
                     <div class="flex flex-row justify-between w-full">
                       <div
-                        class="order-first relative inline-block text-left cursor-pointer shadow-xs"
+                        class="order-first relative inline-block text-left cursor-pointer"
                       >
                         <DropdownMenu.Root>
                           <DropdownMenu.Trigger asChild let:builder>
                             <Button
                               builders={[builder]}
-                              class="w-full border-gray-300 font-semibold dark:font-normal dark:border-gray-600 border bg-black  text-white dark:text-black dark:bg-white dark:sm:hover:bg-gray-100  ease-out  flex flex-row justify-between items-center px-3 py-2  rounded truncate"
+                              class="w-full bg-white dark:bg-secondary text-gray-800 sm:hover:text-black dark:text-gray-200 dark:sm:hover:text-white ease-out flex flex-row justify-between items-center  rounded truncate"
                             >
-                              <span class="truncate">@Agents</span>
                               <svg
-                                class="-mr-1 ml-3 h-5 w-5 xs:ml-2 inline-block"
-                                viewBox="0 0 20 20"
-                                fill="currentColor"
-                                style="max-width:40px"
-                                aria-hidden="true"
-                              >
-                                <path
+                                class="size-4.5"
+                                viewBox="0 0 15 15"
+                                fill="none"
+                                xmlns="http://www.w3.org/2000/svg"
+                                ><path
+                                  d="M5.5 3C4.67157 3 4 3.67157 4 4.5C4 5.32843 4.67157 6 5.5 6C6.32843 6 7 5.32843 7 4.5C7 3.67157 6.32843 3 5.5 3ZM3 5C3.01671 5 3.03323 4.99918 3.04952 4.99758C3.28022 6.1399 4.28967 7 5.5 7C6.71033 7 7.71978 6.1399 7.95048 4.99758C7.96677 4.99918 7.98329 5 8 5H13.5C13.7761 5 14 4.77614 14 4.5C14 4.22386 13.7761 4 13.5 4H8C7.98329 4 7.96677 4.00082 7.95048 4.00242C7.71978 2.86009 6.71033 2 5.5 2C4.28967 2 3.28022 2.86009 3.04952 4.00242C3.03323 4.00082 3.01671 4 3 4H1.5C1.22386 4 1 4.22386 1 4.5C1 4.77614 1.22386 5 1.5 5H3ZM11.9505 10.9976C11.7198 12.1399 10.7103 13 9.5 13C8.28967 13 7.28022 12.1399 7.04952 10.9976C7.03323 10.9992 7.01671 11 7 11H1.5C1.22386 11 1 10.7761 1 10.5C1 10.2239 1.22386 10 1.5 10H7C7.01671 10 7.03323 10.0008 7.04952 10.0024C7.28022 8.8601 8.28967 8 9.5 8C10.7103 8 11.7198 8.8601 11.9505 10.0024C11.9668 10.0008 11.9833 10 12 10H13.5C13.7761 10 14 10.2239 14 10.5C14 10.7761 13.7761 11 13.5 11H12C11.9833 11 11.9668 10.9992 11.9505 10.9976ZM8 10.5C8 9.67157 8.67157 9 9.5 9C10.3284 9 11 9.67157 11 10.5C11 11.3284 10.3284 12 9.5 12C8.67157 12 8 11.3284 8 10.5Z"
+                                  fill="currentColor"
                                   fill-rule="evenodd"
-                                  d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
                                   clip-rule="evenodd"
-                                ></path>
-                              </svg>
+                                ></path></svg
+                              >
+                              <div class="flex items-center gap-0.5">
+                                <span class="max-w-16 truncate"
+                                  ><span class="truncate"></span></span
+                                >
+                              </div>
                             </Button>
                           </DropdownMenu.Trigger>
                           <DropdownMenu.Content
@@ -573,7 +586,95 @@
                             class="w-64 h-fit max-h-56 overflow-y-auto scroller"
                           >
                             {#if selectedGroup === "overview"}
+                              <DropdownMenu.Label
+                                class="text-muted dark:text-gray-400 font-semibold dark:font-normal text-xs"
+                              >
+                                {data?.user?.credits} Credits left
+                              </DropdownMenu.Label>
+
+                              <DropdownMenu.Item
+                                class="sm:hover:bg-gray-200 dark:sm:hover:bg-primary"
+                              >
+                                <label
+                                  on:click|capture={(event) => {
+                                    event.preventDefault();
+                                    $chatReasoning = !$chatReasoning;
+                                    saveSettings();
+                                  }}
+                                  class="inline-flex justify-between w-full items-center cursor-pointer"
+                                >
+                                  <span class="mr-1 text-sm">Reasoning</span>
+
+                                  <div class="relative ml-auto">
+                                    <input
+                                      type="checkbox"
+                                      checked={$chatReasoning}
+                                      class="sr-only peer"
+                                    />
+                                    <div
+                                      class="w-9 h-5 bg-gray-400 rounded-full peer peer-checked:bg-blue-600
+      after:content-[''] after:absolute after:top-0.5 after:left-[2px]
+      after:bg-white after:border-gray-300 after:border
+      after:rounded-full after:h-4 after:w-4 after:transition-all
+      peer-checked:after:translate-x-full"
+                                    ></div>
+                                  </div></label
+                                >
+                              </DropdownMenu.Item>
+
                               <DropdownMenu.Group>
+                                <DropdownMenu.Item
+                                  on:click={(e) => {
+                                    e.preventDefault();
+                                    selectedGroup = "stockAgents";
+                                  }}
+                                  class="cursor-pointer sm:hover:bg-gray-300 dark:sm:hover:bg-primary"
+                                >
+                                  <div
+                                    class="flex flex-row items-center w-full text-sm"
+                                  >
+                                    <span>Stock Agents</span>
+                                  </div>
+                                  <svg
+                                    class="ml-auto h-5 w-5 inline-block rotate-270"
+                                    viewBox="0 0 20 20"
+                                    fill="currentColor"
+                                    style="max-width:40px"
+                                    aria-hidden="true"
+                                  >
+                                    <path
+                                      fill-rule="evenodd"
+                                      d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                                      clip-rule="evenodd"
+                                    ></path>
+                                  </svg>
+                                </DropdownMenu.Item>
+                              </DropdownMenu.Group>
+                            {:else if selectedGroup === "stockAgents"}
+                              <DropdownMenu.Group>
+                                <div
+                                  class="w-full p-1 flex items-stretch gap-1"
+                                >
+                                  <button
+                                    type="button"
+                                    on:click={(e) => {
+                                      e.preventDefault();
+                                      selectedGroup = "overview";
+                                    }}
+                                    class="aspect-square flex items-center cursor-pointer"
+                                    ><svg
+                                      xmlns="http://www.w3.org/2000/svg"
+                                      class="w-5 h-5"
+                                      viewBox="0 0 24 24"
+                                      fill="none"
+                                      stroke="currentColor"
+                                      stroke-width="2"
+                                      stroke-linecap="round"
+                                      stroke-linejoin="round"
+                                      ><path d="m15 18-6-6 6-6"></path></svg
+                                    ></button
+                                  >
+                                </div>
                                 {#each agentCategory as option}
                                   <DropdownMenu.Item
                                     on:click={(e) => {
@@ -640,7 +741,7 @@
                                     type="button"
                                     on:click={(e) => {
                                       e.preventDefault();
-                                      selectedGroup = "overview";
+                                      selectedGroup = "stockAgents";
                                     }}
                                     class="aspect-square flex items-center cursor-pointer"
                                     ><svg
@@ -680,54 +781,31 @@
                           </DropdownMenu.Content>
                         </DropdownMenu.Root>
                       </div>
-                      {#if data?.user}
-                        <label
-                          class="ml-auto mr-2 whitespace-nowrap w-auto text-xs border-gray-300 font-semibold dark:font-normal dark:border-gray-600 border bg-white dark:bg-[#2A2E39] flex flex-row justify-between items-center px-3 rounded"
-                        >
-                          <div>
-                            {data?.user?.credits?.toLocaleString("en-US")}
-                            <span class="hidden sm:inline-block">Credits</span>
-                          </div>
-                        </label>
-                      {/if}
+
+                      <!--
+            {#if data?.user}
+              <label
+                class="ml-auto mr-2 whitespace-nowrap w-auto text-xs border-gray-300 font-semibold dark:font-normal dark:border-gray-600 border bg-white dark:bg-[#2A2E39] flex flex-row justify-between items-center px-3 rounded"
+              >
+                <div>
+                  {data?.user?.credits?.toLocaleString("en-US")}
+                  <span class="hidden sm:inline-block">Credits</span>
+                </div>
+              </label>
+            {/if}
+          -->
 
                       <label
                         for={!data?.user ? "userLogin" : ""}
                         on:click={() => (data?.user ? createChat() : "")}
                         class="{editorText?.trim()?.length > 0
                           ? 'cursor-pointer'
-                          : 'cursor-not-allowed opacity-60'} py-2 text-white dark:text-black text-[1rem] rounded border border-gray-300 dark:border-gray-800 bg-black dark:bg-white px-3 transition-colors duration-200"
+                          : 'cursor-not-allowed opacity-60'} py-2 text-white dark:text-black text-[1rem] rounded border border-gray-300 dark:border-gray-700 bg-black dark:bg-white px-3 transition-colors duration-200"
                       >
                         {#if isLoading}
-                          <svg
-                            class="w-4 h-4 animate-spin text-white dark:text-black"
-                            viewBox="0 0 24 24"
-                            fill="none"
-                          >
-                            <circle
-                              cx="12"
-                              cy="12"
-                              r="10"
-                              stroke="currentColor"
-                              stroke-width="2"
-                              stroke-linecap="round"
-                              stroke-dasharray="31.416"
-                              stroke-dashoffset="31.416"
-                            >
-                              <animate
-                                attributeName="stroke-dasharray"
-                                dur="2s"
-                                values="0 31.416;15.708 15.708;0 31.416"
-                                repeatCount="indefinite"
-                              />
-                              <animate
-                                attributeName="stroke-dashoffset"
-                                dur="2s"
-                                values="0;-15.708;-31.416"
-                                repeatCount="indefinite"
-                              />
-                            </circle>
-                          </svg>
+                          <span
+                            class="loading loading-spinner loading-xs text-center m-auto flex justify-center items-center text-white dark:text-black"
+                          ></span>
                         {:else}
                           <Arrow
                             class="w-4 h-4 text-center m-auto flex justify-center items-center text-white dark:text-black"
